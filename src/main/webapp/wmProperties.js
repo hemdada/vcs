5bc8211f6c2cd3cac6a3f7f67d5b5f08d3751433
Hemanth Reddy <hemanth.sannareddy@wavemaker.com>
2016-08-25 12:22:49 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "vcs",
+  "homePage" : "Main",
+  "name" : "vcs",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
